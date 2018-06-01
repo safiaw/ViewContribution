0209b68a578745a553bf8e046da506f729413742
safiaw <wahdat.safia@gmail.com>
2018-06-01 16:22:17 +0530

    Completed DB request

--- app/src/main/java/com/example7/viewcontribution/MyListAdapter.java
+++ app/src/main/java/com/example7/viewcontribution/MyListAdapter.java
@@ -26,18 +26,19 @@ public class MyListAdapter extends ArrayAdapter<String>
 {
     private Activity Context;
     private String[] maintitle;
-    private String[] subtitle;
+    private Integer[] subtitle;
 
-    public MyListAdapter(Activity context, String[] maintitle, String[] subtitle) {
-        super(context,R.layout.list_view, maintitle);
+
+    public MyListAdapter(Activity context, String[] maintitle, Integer[] subtitle) {
+        super(context,R.layout.list_row_item, maintitle);
         this.Context = context;
         this.maintitle = maintitle;
         this.subtitle = subtitle;
     }
     public View getView(int position, View view, ViewGroup parent)
     {
-        LayoutInflater inflater=Context.getLayoutInflater();
-        View rowView=inflater.inflate(R.layout.list_view, null,true);
+        LayoutInflater inflater = Context.getLayoutInflater();
+        View rowView=inflater.inflate(R.layout.list_row_item, null, true);
 
         TextView titleText = (TextView) rowView.findViewById(R.id.title);
         TextView subtitleText = (TextView) rowView.findViewById(R.id.subtitle);
@@ -48,59 +49,5 @@ public class MyListAdapter extends ArrayAdapter<String>
         return rowView;
 
     }
-    void access_participant_event_data_and_show()
-    {
-        // create JSON object POST request
-        StringRequest participantEventListRequest = new StringRequest(Request.Method.POST, AppConfig.URL_LIST_PARTICIPANT_EVENTS_AND_CONTRBS, new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                json_query_result = response;
-                display_participant_event_along_with_their_contr();
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error){
-                error.printStackTrace();
-            }
-        })
-        {
-            @Override
-            public Map<String, String> getParams(){
-                SessionManager sessionManager = new SessionManager(getApplicationContext());
-                Map<String, String> params = new HashMap<String, String>();
-                params.put("loginToken", sessionManager.getLoginToken());
-                return params;
-            }
-            @Override
-            public Map<String, String> getHeaders() throws AuthFailureError{
-                Map <String, String> params = new HashMap<String, String>();
-                params.put("Content-Type", "application/x-www-form-urlencoded");
-                return params;
-            }
-        };
-        // Insert the created POST request to Volley request queue
-        AppController.getInstance().addToRequestQueue(participantEventListRequest);
-    }
 
-    public void display_participant_event_along_with_their_contr()
-    {
-        // extract information from json result
-        final ArrayList<Map<String, Integer>> eventscontrb = new ArrayList<Map<String, Integer>>();
-        try {
-            JSONObject json_response = new JSONObject(json_query_result);
-            JSONArray json_main_node = json_response.optJSONArray("EventListAndContribution");
-
-            // extract event and Initialize Event array List
-
-            for(int j=0; j< json_main_node.length();j++) {
-                Map<String, Integer> eacheventcontrb = new HashMap<String, Integer>();
-                JSONObject json_child_node = json_main_node.getJSONObject(j);
-                eacheventcontrb.set
-            }
-        }
-
-
-
-
-    }
 }
--- app/src/main/java/com/example7/viewcontribution/ViewEventContribution.java
+++ app/src/main/java/com/example7/viewcontribution/ViewEventContribution.java
@@ -1,5 +1,6 @@
 package com.example7.viewcontribution;
 
+import android.app.DownloadManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,56 +9,106 @@ import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-public class ViewEventContribution extends AppCompatActivity{
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+public class ViewEventContribution extends AppCompatActivity {
 
     private ListView mListView;
-    private
-    String[] maintitle={"Title 1","Title 2","Title 3","Title 4","Title 5"};
-    String[] subtitle={"Sub Title 1","Sub Title 2","Sub Title 3","Sub Title 4","Sub Title 5"};
+    private String[] maintitle1 = {"Title 1", "Title 2", "Title 3", "Title 4", "Title 5"};
+    private String[] subtitle1 = {"Sub Title 1", "Sub Title 2", "Sub Title 3", "Sub Title 4", "Sub Title 5"};
+    private String[] maintitle = {};
+    private Integer[] subtitle = {};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_view_contribution);
 
+        // fetch the data from DB into maintitle and subtitle
+        get_participant_event_data();
+
+
+    }
+
+    // Communicate with DB controller to get particpant event list along with their contribution
+    void get_participant_event_data() {
+
+        // creating a static list of event names and their contribution
+        List<Map<String, Integer>> list = new ArrayList<Map<String, Integer>>();
+        Map<String, Integer> row_map = new HashMap<String, Integer>();
+        row_map.put("Event_name1",1200);
+        list.add(row_map);
+        row_map.put("Event_name2",1400);
+        list.add(row_map);
+        row_map.put("Event_name3",1600);
+        list.add(row_map);
+        row_map.put("Event_name4",1800);
+        list.add(row_map);
+
+        String PartID = "Hello_123";
+
+        // create JSON object POST request
+        StringRequest participantEventListRequest = new StringRequest(DownloadManager.Request.Method.POST, AppConfig.URL_LIST_PARTICIPANT_EVENTS_AND_CONTRBS, new Response.Listener<String>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                json_query_result = response;
+                display_participant_event_along_with_their_contr(json_query_result);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        }) {
+            @Override
+            public Map<String, String> getParams() {
+                SessionManager sessionManager = new SessionManager(getApplicationContext());
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("loginToken", sessionManager.getLoginToken());
+                params.put("ParticipantID", PartID);
+                return params;
+            }
+
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("Content-Type", "application/x-www-form-urlencoded");
+                return params;
+            }
+        };
+
+    }
+    public void display_participant_event_along_with_their_contr(JSONObject json_query_result)
+    {
+        try {
+            JSONObject json_response = new JSONObject(json_query_result);
+            JSONArray json_main_node = json_response.getJSONArray("EventListAndContribution");
+            // extract event and initialize Event array list
+
+            for (int j = 0; j < json_main_node.length(); j++) {
+                JSONObject json_child_node = json_main_node.getJSONObject(j);
+                maintitle[j]= json_child_node.getString("Event_name");
+                subtitle[j]= json_child_node.getInt("Event_contr");
+            }
+        }
+        catch (JSONException e){
+            Toast.makeText(getApplicationContext(), "Error" + e.toString(),
+                    Toast.LENGTH_SHORT).show();
+        }
+        // bind the obtained values to the adapter
         MyListAdapter adapter = new MyListAdapter(this, maintitle, subtitle);
         mListView = (ListView) findViewById(R.id.list_event);
         mListView.setAdapter((ListAdapter) adapter);
-        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                //Code specific to first list item
-                if(position==0)
-                {
-                    Toast.makeText(getApplicationContext(),"First List Item",Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
 
-
-        //list_events_and_show();
     }
 
-    //Generate DB request to get event list and then render it
-  /**  private void list_events_and_show() {
-       // Get list of events in which the volunteer has participated from DB corresponding to Volunteers ID
-
-
-
-        // Populate the list with the events
-        // On click of each item in the list should show contribution details
-
-
-
-        mListView = (ListView) findViewById(R.id.listevents);
-
-        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, listItems);
-        mListView.setAdapter(adapter);
-        display_event_contribution();
-    }
-
-    private void display_event_contribution() {
-    }**/
 
 }
